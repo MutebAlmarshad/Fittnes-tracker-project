d93d40288c0cca75a1b82ccabd7234bc4e499e82
MutebAlmarshad <muteb11almarshad@gmail.com>
2024-03-29 07:23:00 +0300

    adding tables

--- fitWebsite/apps/fitnessApp/urls.py
+++ fitWebsite/apps/fitnessApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, re_path
 from apps.fitnessApp import views
 
 urlpatterns = [    
-#    path('', views.index, name='index'),
+   path('', views.index, name='index'),
+   path('workout', views.workout, name='workout'),
+   path('diet', views.diet, name='diet'),
 ]
--- fitWebsite/apps/fitnessApp/views.py
+++ fitWebsite/apps/fitnessApp/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render, redirect
+
+def index(request):
+	return render(request, 'fitWeb/index.html')
+
+def workout(request):
+	return render(request, 'fitWeb/workout.html')
+
+def diet(request):
+	return render(request, 'fitWeb/diet.html')
